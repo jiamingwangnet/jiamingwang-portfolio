9028fb1d02b84cc2a349b39bbb27e2b8f10ab882
jiamingwangnet <jiamingwang.net@gmail.com>
2024-07-10 21:13:26 +1000

    404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,17 @@
+import { Manrope } from "next/font/google";
+import Link from "next/link";
+
+const manrope = Manrope({
+    subsets: ["latin"]
+});
+
 export default function NotFound()
 {
-    return <h1>404</h1>;
+    return <div className="fixed top-0 left-0 w-screen h-screen flex justify-center items-center">
+        <div>
+            <h1 className={`text-9xl ${manrope.className}`}>404</h1>
+            <p className="text-center text-xl">Page not found</p>
+            <Link href="/" className="block w-full text-center text-highlight-2">Home</Link>
+        </div>
+    </div>;
 }
