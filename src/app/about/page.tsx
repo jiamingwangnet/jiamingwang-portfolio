2f143185b387104675fb9fdb5bf4ade17c11b145
jiamingwangnet <jiamingwang.net@gmail.com>
2024-07-11 15:03:11 +1000

    image for static site

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,7 @@
 import "../globals.css"
 import Image from "next/image";
-import photo from "@/resources/IMG_2429.jpg";
+// does not work for static sites
+// import photo from "@/resources/IMG_2429.jpg";
 import { Manrope } from 'next/font/google';
 import Heading from "@/components/Heading/Heading";
 import { Metadata } from 'next';
@@ -21,7 +22,7 @@ export default function About()
                 <main className="contentHolder bg-bg-color/90 flex items-center justify-center overflow-y-auto overflow-x-hidden">
                     <div className="relative w-fit flex flex-row-reverse items-center justify-center flex-wrap md:flex-nowrap md:h-full">
                         <div className="m-5 fly-in-r-l lg:w-1/3 md:w-1/2 w-full h-1/2 md:h-auto">
-                            <Image src={photo} alt="Photo of my piano" 
+                            <Image src="/assets/images/IMG_2429.jpg" alt="Photo of my piano" 
                             className="md:w-full md:max-h-[550px] h-full rounded-lg object-cover" priority/>
                         </div>
                         <div className="fly-in-l-r lg:w-2/3 md:w-1/2 h-2/6 md:h-auto">
