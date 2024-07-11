37e56e625bd060807b32178a91746d1ac8b668d6
jiamingwangnet <jiamingwang.net@gmail.com>
2024-07-11 20:38:34 +1000

    update

---
+++ src/components/GoogleAnalytics/GoogleAnalytics.tsx
@@ -0,0 +1,25 @@
+import Script from "next/script";
+
+export default function GoogleAnalytics()
+{
+    return (
+        <>
+            <Script
+                strategy="lazyOnload"
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+            />
+
+            <Script id='' strategy="lazyOnload">
+                {
+                    `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', '${process.env.GOOGLE_ANALYTICS}');
+                    `
+                }
+            </Script>
+        </>
+    )
+}
