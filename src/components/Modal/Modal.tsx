7bbfab9f848014d1704a301af17ad846cecf251b
jiamingwangnet <jiamingwang.net@gmail.com>
2024-07-13 00:27:28 +1000

    modal fix

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -9,7 +9,7 @@ export default function Modal({children, zindex=70}:{children:React.ReactNode,zi
     const router = useRouter();
 
     return (
-        <div className={`fixed top-0 left-0 w-screen h-screen bg-black/60 flex justify-center items-center`} style={{zIndex: zindex, animation:
+        <div className={`fixed top-0 left-0 w-full h-full bg-black/60 flex justify-center items-center`} style={{zIndex: zindex, animation:
             "fadeIn 380ms cubic-bezier(.26,.84,.4,1.01) 0ms forwards"
         }}
             onClick={e => {
@@ -22,8 +22,8 @@ export default function Modal({children, zindex=70}:{children:React.ReactNode,zi
                     e.stopPropagation();
                     e.nativeEvent.stopImmediatePropagation();
                 }}
-                className="relative p-3 h-[calc(100%-50px)] lg:w-2/3 md:w-10/12 w-[calc(100%-20px)] bg-bg-color shadow-2xl border border-white/20">
-                <span className="absolute right-4 text-3xl w-8 h-8 transition hover:bg-white/30 text-center rounded-full cursor-pointer"
+                className="relative p-3 h-[calc(100%-50px)] lg:w-2/3 md:w-10/12 w-[calc(100%-20px)] bg-bg-color shadow-xl shadow-slate-black border border-white/20">
+                <span className="absolute right-4 text-3xl w-8 h-8 transition hover:bg-white/30 text-center rounded-full cursor-pointer z-[90]"
                     onClick={e => {
                         router.back();
                     }}
