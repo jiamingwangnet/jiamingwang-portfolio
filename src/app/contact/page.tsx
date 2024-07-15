b2fbb27dcb5fc3778eb34109999162eab71663ce
jiamingwangnet <jiamingwang.net@gmail.com>
2024-07-15 17:28:19 +1000

    animation fix

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -13,8 +13,8 @@ export default function Contact()
         <div className="w-11/12 mr-auto ml-auto mt-3 trContent">
             <Heading style={{animation: FlyInRL()}}>Contact</Heading>
             <ul>
-                <li style={{animation: FlyInRL("100ms")}}>Email: <OutboundLink href="mailto:jiamingwang.net@gmail.com">jiamingwang.net@gmail.com</OutboundLink></li>
-                <li style={{animation: FlyInRL("150ms")}}>Github: <OutboundLink href="https://github.com/jiamingwangnet">github.com/jiamingwangnet</OutboundLink></li>
+                <li style={{animation: FlyInRL("100ms"), transform:"translateX(100vw)"}}>Email: <OutboundLink href="mailto:jiamingwang.net@gmail.com">jiamingwang.net@gmail.com</OutboundLink></li>
+                <li style={{animation: FlyInRL("150ms"), transform:"translateX(100vw)"}}>Github: <OutboundLink href="https://github.com/jiamingwangnet">github.com/jiamingwangnet</OutboundLink></li>
             </ul>
         </div>
     </>
